0febb8f6ef23c9cd892f3e4888e50fbda55640aa
Cyborg-Dino <dinosaurrr999@gmail.com>
2022-11-29 15:45:32 +0300

    init

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 public class Main {
+
     public static void main(String[] args) {
         System.out.print("I'm glad to welcome you here.");
     }
---
+++ mycode.java
@@ -0,0 +1,5 @@
+public class mycode {
+    public static void main(String[] args) {
+        System.out.print("Mesa called Jar Jar Binks, mesa your humble servant!");
+    }
+}
