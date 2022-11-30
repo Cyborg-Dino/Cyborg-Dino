a331dcf92491e1041657a073c62476ddf28052a3
Cyborg-Dino <119415190+Cyborg-Dino@users.noreply.github.com>
2022-11-30 12:59:12 +0300

    Create Solutionfour.java

---
+++ Solutionfour.java
@@ -0,0 +1,6 @@
+public class Solutionfour {
+    public static void main(String[] args) {
+        String cyborg = "Easy to learn if you follow the instructions";
+        System.out.printf("%s", cyborg);
+    }
+}
