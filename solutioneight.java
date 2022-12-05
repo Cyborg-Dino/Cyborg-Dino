354f967350b5a8e187c331b2bbe8e71e05e21ffa
Cyborg-Dino <119415190+Cyborg-Dino@users.noreply.github.com>
2022-12-05 15:15:15 +0300

    Create solutioneight.java

---
+++ solutioneight.java
@@ -0,0 +1,7 @@
+public class solutioneight {
+    public static void main(String[] agrs) {
+        String cat = "kitten";
+        String text = "Cute " + cat + "!";
+        System.out.println(text);
+    }
+}
