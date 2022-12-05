58fb1fc4857a6a78febdf0e6f245c4be6b93f951
Cyborg-Dino <119415190+Cyborg-Dino@users.noreply.github.com>
2022-12-05 15:28:53 +0300

    Create solutionnine.java

---
+++ solutionnine.java
@@ -0,0 +1,15 @@
+public class solutionnine {
+    public static void main(String[] agrs) {
+        // int a = 1;
+        double b = 1.5;
+        double c = b + 1.5;
+        // int d = a + 12;
+        // double e = 12.3;
+        // String s = "Like, " + a;
+        String s1 = "Twice ";
+        // String s2 = "a";
+        String s3 = s1 + "the pride, ";
+        String s4 = " the fall.";
+        System.out.println(s3 + c + s4);
+    }
+}
