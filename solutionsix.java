d4f99831d4b5bc7bd8d9793b227943abe7841360
Cyborg-Dino <119415190+Cyborg-Dino@users.noreply.github.com>
2022-12-05 15:02:56 +0300

    Create solutionsix.java

---
+++ solutionsix.java
@@ -0,0 +1,9 @@
+public class solutionsix {
+    public static void main(String[] args) {
+        System.out.println(sqr(5));
+    }
+
+    public static int sqr(int a) {
+        return a * a;
+    }
+}
