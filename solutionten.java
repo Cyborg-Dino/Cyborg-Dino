6f01698301364d75bee108af7a518c0e08c31bc8
Cyborg-Dino <119415190+Cyborg-Dino@users.noreply.github.com>
2022-12-08 14:46:59 +0300

    Create solutionten.java

---
+++ solutionten.java
@@ -0,0 +1,13 @@
+public class solutionten {
+    public static void main(String[] args) {
+        print("The power is easy to use!");
+        print("The power opens many opportunities!");
+    }
+
+    public static void print(String s) {
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+    }
+}
