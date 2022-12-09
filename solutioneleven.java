ce19e8f296fa8edaceacfed2769ff593409e636b
Cyborg-Dino <119415190+Cyborg-Dino@users.noreply.github.com>
2022-12-10 02:02:36 +0300

    Create solutioneleven.java

---
+++ solutioneleven.java
@@ -0,0 +1,12 @@
+public class solutioneleven {
+    public static void main(String[] args) {
+        count(105);
+    }
+
+    public static void count(int n) {
+        System.out.println("Первая степень числа " + n + " = " + n);
+        System.out.println("Квадрат числа " + n + " = " + n * n);
+        System.out.println("Куб числа " + n + " = " + n * n * n);
+        System.out.println("Четвёртая степень числа " + n + " = " + n * n * n * n);
+    }
+}
