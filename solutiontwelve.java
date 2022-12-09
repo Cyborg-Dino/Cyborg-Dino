f7217a1f25758c3448fbe0d328b9047955854e05
Cyborg-Dino <119415190+Cyborg-Dino@users.noreply.github.com>
2022-12-10 02:28:55 +0300

    Create solutiontwelve.java

---
+++ solutiontwelve.java
@@ -0,0 +1,7 @@
+public class solutiontwelve {
+    public static void main(String[] args) {
+        String name = "Cyborg";
+        String hobby = "draw";
+        System.out.println("Hello my future employers, my name is " + name + " and my hobby is to " + hobby);
+    }
+}
