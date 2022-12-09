a8b8d93bbff448792ef9b9c6e8109241041271fd
Cyborg-Dino <119415190+Cyborg-Dino@users.noreply.github.com>
2022-12-10 02:55:04 +0300

    Create solutionthirteen.java

---
+++ solutionthirteen.java
@@ -0,0 +1,9 @@
+public class solutionthirteen {
+    public static void main(String[] args) {
+        say("Sonya", "red");
+    }
+
+    static void say(String name, String color) {
+        System.out.println("My cat's name is " + name + " and she is " + color);
+    }
+}
