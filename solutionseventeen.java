aea5c1c1a68a216eeb4fbc83e5f3fc0aa75cbb02
Cyborg-Dino <119415190+Cyborg-Dino@users.noreply.github.com>
2022-12-12 17:50:36 +0300

    Create solutionseventeen.java

---
+++ solutionseventeen.java
@@ -0,0 +1,10 @@
+public class solutionseventeen {
+    public static void print3(String s) {
+        System.out.println(s + " " + s + " " + s + " ");
+    }
+
+    public static void main(String[] args) {
+        print3("damp");
+        print3("cargo");
+    }
+}
