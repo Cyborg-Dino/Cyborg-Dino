fb76823b2baccb668c3f394d77ec7f6d69878663
Cyborg-Dino <119415190+Cyborg-Dino@users.noreply.github.com>
2022-12-12 03:35:12 +0300

    Create olutionsixteen.java

---
+++ olutionsixteen.java
@@ -0,0 +1,9 @@
+public class solutionsixteen {
+    public static void main(String[] args) {
+        System.out.println(getWeight(888));
+    }
+
+    public static double getWeight(int weight) {
+        return (weight * 0.17);
+    }
+}
