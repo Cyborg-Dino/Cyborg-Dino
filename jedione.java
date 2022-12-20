aad814464fe3b6eba41ce66483503b1f5529286e
Cyborg-Dino <119415190+Cyborg-Dino@users.noreply.github.com>
2022-12-21 02:32:05 +0300

    Create jedione.java

---
+++ jedione.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class jedione {
+    public static void main(String[] args) throws Exception {
+        int jedis = 212;
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        System.out.println(jedis - number);
+    }
+}
