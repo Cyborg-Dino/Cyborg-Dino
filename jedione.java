94fadf7853bf0c58e9d8120c35ac34570c5919b1
Cyborg-Dino <119415190+Cyborg-Dino@users.noreply.github.com>
2022-12-21 20:51:07 +0300

    Update jedione.java

--- jedione.java
+++ jedione.java
@@ -3,8 +3,9 @@ import java.util.*;
 public class jedione {
     public static void main(String[] args) throws Exception {
         int jedis = 212;
-        Scanner scanner = new Scanner(System.in);
-        int number = scanner.nextInt();
-        System.out.println(jedis - number);
+        try (Scanner scanner = new Scanner(System.in)) {
+            int number = scanner.nextInt();
+            System.out.println(jedis - number);
+        }
     }
 }
