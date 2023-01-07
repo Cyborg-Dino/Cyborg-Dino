a319b85af9fada81e18b2c426e09bfedd578f0e9
Cyborg-Dino <119415190+Cyborg-Dino@users.noreply.github.com>
2023-01-07 17:27:34 +0300

    Create height.java

---
+++ height.java
@@ -0,0 +1,7 @@
+public class height {
+    public static void main(String[] args) throws Exception {
+        int num = 100;
+        while (num >= 0)
+            System.out.println(num--);
+    }
+}
