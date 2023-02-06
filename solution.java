f64f0c6553d43b2dd0bf9d120bd1959a45415cfa
Cyborg-Dino <119415190+Cyborg-Dino@users.noreply.github.com>
2023-02-06 23:38:52 +0300

    Create solution.java

---
+++ solution.java
@@ -0,0 +1,24 @@
+public class solution {
+    String name;
+    int count;
+    String weapon;
+
+    public void initialize(String name) {
+        this.name = name;
+    }
+
+    public void initialize(String name, int count) {
+        this.name = name;
+        this.count = count;
+    }
+
+    public void initialize(String name, int count, String weapon) {
+        this.name = name;
+        this.count = count;
+        this.weapon = weapon;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
