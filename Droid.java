882c2aa299ecff6c6bc6dde07fdd36f1902e1c08
Cyborg-Dino <119415190+Cyborg-Dino@users.noreply.github.com>
2023-02-06 23:58:01 +0300

    Create Droid.java

---
+++ Droid.java
@@ -0,0 +1,17 @@
+public class Droid {
+    String name;
+    String model;
+
+    public Droid(String name) {
+        this.name = name;
+    }
+
+    public Droid(String name, String model) {
+        this.name = name;
+        this.model = model;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
