e887fcd6bb290bd7caf680ef240965eae6052a2d
Cyborg-Dino <119415190+Cyborg-Dino@users.noreply.github.com>
2023-03-29 16:11:38 +0300

    Create Tunnel.java

---
+++ Tunnel.java
@@ -0,0 +1,22 @@
+public class Tunnel {
+    private int centerX, centerY, radius, width, color;
+
+    public void initialize(int centerX, int centerY) {
+        this.centerX = centerX;
+        this.centerY = centerY;
+
+    }
+
+    public void initialize (int centerX, int centerY, int radius) {
+        this.centerX;
+        this.centerY;
+        this.radius;
+
+    }
+
+    public void initialize (int centerX, int centerY, int radius, int width) {
+        this.centerX;
+        this.centerY;
+        this.radius;
+        this.width;
+    }
